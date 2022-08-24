aca97713084a31883ddff03376ebe798009ab921
braije <idworkx@gmail.com>
2022-08-24 21:41:07 +0200

    Clearnup

--- index.js
+++ index.js
@@ -6,37 +6,16 @@ console.clear();
 
 const $ = require('betiny-core');
 
-// !!! IMPORTANT !!!!
-// Custom middleware should be setup before any route.
-$.middleware.add('myCustomMiddleware', 200,(req, res, next) => {
-  if (req.betiny.xxx) {
-    console.log("\n");
-    $.log.top("myCustomMiddleware");
-    $.log.child("zzz ->", req.betiny.zzz);
-    $.log.end("xxx ->", req.betiny.xxx);
-  }
-  next();
-});
-
 // Self organisation.
 require('./app/html2m');
 require('./app/nuts');
 
 // Custom root route.
-$.route.get('/', { xxx: "yeahhh", zzz: "what?" }, (req, res) => {
-  res.send("Welcome to my custom server.");
-});
-
-// Custom command.
-$.arguments.add("custom:command", params => {
-  $.log.end("custom:command was running with these params", params);
-  process.exit();
+$.route.get('/', (req, res) => {
+  res.send("Welcome to Webtools.");
 });
 
 // Self start management.
-$.server.start(() => {
-
-  // Custom events.
-  $.fire('ready');
-
+$.server.start( () => {
+  $.fire("ready");
 });
